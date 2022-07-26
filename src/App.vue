efaf8b1448fef62f09d8af2606e67a2a759cfca6
ChingC <chingc.ccmail@gmail.com>
2022-07-26 10:59:37 +0800

    feat: first version

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { NMessageProvider } from 'naive-ui';
 import Editor from './components/Editor.vue'
 </script>
 
 <template>
-  <Editor />
+  <NMessageProvider>
+    <Editor />
+  </NMessageProvider>
 </template>
 
 <style lang="less" scoped>
